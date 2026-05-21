4a2a8dc072471008daa57dda4ef6f28775d75fcd
glebzelinskij <it.develop.ldubgd@outlook.com>
2026-05-21 15:23:04 +0300

    Initial Spring Boot setup

---
+++ src/main/java/com/gleb/swimshop/SwimShopApplication.java
@@ -0,0 +1,13 @@
+package com.gleb.swimshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gleb/swimshop/SwimShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleb.swimshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
