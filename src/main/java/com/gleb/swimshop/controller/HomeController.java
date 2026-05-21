c2b17d74e6a8a69a9b25f5baef5c22bd7c55810f
Gelbpi <glebzelinskiy1206@gmail.com>
2026-05-21 15:52:30 +0300

    Clean project and add gitignore

---
+++ src/main/java/com/gleb/swimshop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.gleb.swimshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Swim Shop працює!";
+    }
+}
